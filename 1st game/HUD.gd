2249929571154ffbd869ffc8752ab18299bc547e
Fireteen69 <THodges27@bfhsla.org>
2024-10-08 12:09:15 -0500

    Update HUD.gd

--- 1st game/HUD.gd
+++ 1st game/HUD.gd
@@ -9,7 +9,7 @@ func show_game_over():
 	# Wait until the MessageTimer has counted down.
 	await $MessageTimer.timeout
 
-	$Message.text = "Dodge the Creeps!"
+	$Message.text = "Catch The Fruit!"
 	$Message.show()
 	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
